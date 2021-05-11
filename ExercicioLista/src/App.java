91946394637a37a0affbef00e6630e050a073a48
Andressa Soares Rodrigues <rodrigues.andressasoares@gmail.com>
2021-05-11 00:43:21 -0300

    adicionando uma verificação de id de funcionário, para executar um aumento no valor do salário. se o id do funcionário não existir, o programa reporta um erro.

--- ExercicioLista/src/App.java
+++ ExercicioLista/src/App.java
@@ -1,15 +1,17 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 //import jdk.internal.module.SystemModuleFinders;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Locale.setDefault(new Locale("pt", "BR"));
         Scanner sc = new Scanner(System.in);
 
         List <Empregado> list = new ArrayList<>();
-        System.out.println("quantos funcionários serão cadastrados?");
+        System.out.println("Quantos funcionários serão cadastrados?");
         int numFunc = sc.nextInt();
 
         for (int i = 0; i <numFunc; i++) {
@@ -26,7 +28,28 @@ public class App {
 
             list.add(epm);
         }
+        
+        System.out.println("Informe o id do funcionário que terá seu salário aumentado: ");
+        Integer idsalario = sc.nextInt();
+        Integer pos = posicao(list, idsalario);
+        if(pos == null){
+            System.out.println("Este id não existe");
+        } else {
+            System.out.println("Informe a porcentagem de aumento: ");
+            double porcentagem = sc.nextDouble();
+            list.get(pos).aumentaSalario(porcentagem);
+        }
+
 
         sc.close();
+    }
+
+    public static Integer posicao(List<Empregado> list, int id) {
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i).getId() == id){
+                return i;
+            } 
+        }
+        return null;
     }
 }
